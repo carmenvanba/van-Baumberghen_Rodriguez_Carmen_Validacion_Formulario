687fb319442cd79ae94c611bdcc70a8164cc41ad
carmenvanba <114095456+carmenvanba@users.noreply.github.com>
2022-10-10 14:31:06 +0200

    Validaciones campo a campo, hasta email

--- js/formscript.js
+++ js/formscript.js
@@ -1,33 +1,47 @@
-/*
-function validation(){
-    //console.log("Thank you for your click.");
-    window.location.reload();
-}
-*/
+//Author: Carmen van-Baumberghen Rodríguez
+//Date: 10/10/2022
+//Validacion Formulario
 
 function validation(){
-    //validacion
-    //cada validacion erronea que active 1 identificador para saber qué mensaje mandar
-        //si va bien:
-            //console.log("Thank you for your click.");
-            window.location.reload();
-        //sino, mensaje de error correcto
-        //switch para cada error????? ocn un identificador, trycatch mejor
-}
+    var error;
+    var valorCampo;
+    //Iterar para vaciar el texto de error de los inputs
+    Array.from(document.getElementsByClassName("error"))
+    //Parametros: id error, indice, elementos clase error
+    .forEach(function(element, index, array) {
+        resetError(element, index, array)
+    });
+    //Fin ForEach
+
+    if(document.getElementById("name").value.length==0){
+        error = document.getElementById("name_error");
+        error.textContent="El nombre está vacío.";
+    }//Fin Si
+    if(document.getElementById("surnames").value.length==0){
+        error = document.getElementById("surnames_error");
+        error.textContent="Los apellidos están vacíos.";
+    }//Fin Si
 
 
-//se modifica el myAlert
-function myAlert(msg){
-    var div = document.createElement("div");
-    div.classList.add("alert");
-    div.innerHTML = msg;
-    //se añaden las cositas
-    var close = document.createElement("span");
-    //esto debería ir en el css .close
-    //close.style.float = "right";
-    close.classList.add("close");
-    close.innerHTML = "X";
-    div.appendChild(close);
-    $("body").insertBefore(div,$("body").firstChild);
-    bind_close();
-}
+
+    if(document.getElementById("email").value.length==0){
+        error = document.getElementById("email_error");
+        error.textContent="La dirección de correo electrónica está vacía.";
+    }else{
+        //Comprobar si el valor del campo coincide con el patron
+        valorCampo = document.getElementById("email").value;
+        if(!(/[\w\.-]+@[\w\.-]+\.\w{2,4}/.test(valorCampo))){
+            error = document.getElementById("email_error");
+            error.textContent="El formato de la dirección de correo electrónico debe cumplir el siguiente formato: ejemplo@email.com";
+        }//Fin Si
+    }//Fin Si
+
+
+
+
+
+}//Fin Funcion validation
+
+function resetError(element, index, array) {
+    element.textContent="";
+}//Fin Funcion resetError
